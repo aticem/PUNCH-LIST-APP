fdf64e59c7cedef35c6b3b919b15660bebe54917
aticem <65057931+aticem@users.noreply.github.com>
2025-10-27 09:33:55 +0000

    works

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-    cors: true,
-    strictPort: true,
-    allowedHosts: [
-      ".webcontainer.io",
-      ".local-corp.webcontainer.io",
-      ".github.dev",
-      "localhost",
-      "127.0.0.1"
-    ],
-    headers: {
-      "Cross-Origin-Embedder-Policy": "unsafe-none",
-      "Cross-Origin-Opener-Policy": "unsafe-none"
-    }
-  }
+  server: { host: true, port: 5173 }
 });
